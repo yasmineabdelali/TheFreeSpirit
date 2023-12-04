d169c853c11749217b8adc2f0129e15c66680146
mahmoudsalhi <salhimahmoud135@outlook.fr>
2023-12-04 17:45:42 +0100

    Remove all files

--- views/deleteevent.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "../controller/eventC.php";
-$EventC = new eventC();
-$EventC->supp_evenement($_GET["id"]);
-header('Location:dash.php');
-?>
--- views/deleteuserF.php
+++
@@ -1,5 +0,0 @@
-<?php
-include '../controller/userF.php';
-$connexion = new userF();
-$connexion->deleteuserF($_GET["iduserF"]);
-header('Location:listuserFs.php');
